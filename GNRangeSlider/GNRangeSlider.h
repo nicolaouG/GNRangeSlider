31e3f403af972920e30fc85a3387c586ddfe5d2b
george <gnicolaou@axiomc.com>
2020-04-30 10:25:40 +0300

    initial project setup

---
+++ GNRangeSlider/GNRangeSlider.h
@@ -0,0 +1,19 @@
+//
+//  GNRangeSlider.h
+//  GNRangeSlider
+//
+//  Created by george on 30/04/2020.
+//  Copyright © 2020 George Nicolaou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GNRangeSlider.
+FOUNDATION_EXPORT double GNRangeSliderVersionNumber;
+
+//! Project version string for GNRangeSlider.
+FOUNDATION_EXPORT const unsigned char GNRangeSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GNRangeSlider/PublicHeader.h>
+
+
